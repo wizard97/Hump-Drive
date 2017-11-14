d2cd2a25ba6621ec9fc7700891b26922842fe368
sdd48 <sdd48@cornell.edu>
2017-11-14 01:21:16 -0500

    Started crypto

---
+++ crypto.ml
@@ -0,0 +1,21 @@
+(* Scott Dickson (sdd48)
+ * Charles Yu (ccy27)
+ * Zack Brienza (zb5)
+ * Aaron Wisner (daw268)
+*)
+
+
+let key_from_string s =
+  failwith "Unmplemented"
+
+let init_cypher k =
+  failwith "Unimplemented"
+
+let generate_key =
+  failwith "Unimplemented"
+
+let encrypt_line s k =
+  failwith "Unimplemented"
+
+let decrypt_line s pu pr =
+  failwith "Unimplemented"
