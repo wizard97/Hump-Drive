b1b26526d21c98b623587d9b61256ead5e1255d2
Aaron Wisner <daw268@cornell.edu>
2017-12-02 18:30:16 -0500

    Read entire file

--- filetransfer.ml
+++ filetransfer.ml
@@ -20,31 +20,35 @@ let server_add_file f = Server.serve_existing_static_file f
 let client_connect host = Client.connect ~host:(host) ~port:(port)
 
 
-let rec dq_str q =
+let rec dq_str q saveloc =
+  let save_chunk = function
+    | Client.Message.String s -> File_writer.write saveloc s
+    | Client.Message.Bigstring bs -> File_writer.write_bigstring saveloc bs
+  in
   match (Core_kernel.Std.Queue.dequeue q) with
   | None -> ()
   | Some resp ->
     match (resp) with
-    | Error e -> ()
-    | Ok msg -> let () = print_string (Client.Message.to_string_exn msg) in dq_str q
+    | Error e -> print_string "Someone messed up"
+    | Ok msg -> save_chunk msg; dq_str q saveloc
 
-let client_read client fname =
-  let cr = Client.read client fname in
-  let pr = cr >>= fun pipe -> Async_extra.Import.Pipe.read' pipe in
-  let res = pr >>= fun rd -> match rd with
-    | `Eof -> let () = print_string "EOF" in Async_extra.Import.return ()
-    | `Ok q -> let () = dq_str q in Async_extra.Import.return ()
+
+let client_read client fname sfile =
+  let rec readentire pipe =
+    let pread = Async_extra.Import.Pipe.read' pipe in
+    pread >>= fun pq ->
+    match (pq) with
+    | `Eof -> File_writer.close sfile
+    | `Ok q -> dq_str q sfile; readentire pipe
   in
-  res
+  let dpipe = Client.read client fname in
+  dpipe >>= fun pipe -> readentire pipe
 
 
 (* c is client instance, fname is file to read, saveloc is full path*)
 let client_get_file c fname saveloc =
-  File_writer.create saveloc >>= fun f ->
-  File_writer.write f "";
-  File_writer.close f in
-
+  File_writer.create saveloc >>= fun sfile ->
   c >>= fun (res) ->
   match res with
-  | Ok c -> let _ = (client_read c fname) in Async_extra.Import.return (FT_Success ())
+  | Ok c -> let _ = (client_read c fname sfile) in Async_extra.Import.return (FT_Success ())
   | Error r -> Async_extra.Import.return (FT_Error "Error transfering file")
