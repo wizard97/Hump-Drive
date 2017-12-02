84735819898e1ed4b930693cf584f38b50d8a5e5
Aaron Wisner <daw268@cornell.edu>
2017-12-02 18:54:47 -0500

    Fix makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 compile:
-	ocamlbuild -use-ocamlfind communicator.cmo crypto.cmo gui.cmo transfer.cmo
+	ocamlbuild -use-ocamlfind communicator.cmo crypto.cmo gui.cmo filetransfer.ml
 crypto_test:
 	ocamlbuild -use-ocamlfind crypto_test.byte && ./crypto_test.byte
+net_test:
+	ocamlbuild -use-ocamlfind net_test.byte && ./net_test.byte
 clean:
 	ocamlbuild -clean
---
+++ filetransfer.mli
@@ -0,0 +1,23 @@
+open Async
+open Async.Tcp_file
+open Async_extra
+
+type 'a result = FT_Error of string | FT_Success of 'a
+
+
+val port : int
+
+type ftclient =
+  (Tcp_file.Client.t, Base.Exn.t) Core.Result.t Async_extra.Import.Deferred.t
+
+(* Server stuff *)
+val create_server : unit -> Tcp.Server.inet Import.Deferred.t
+
+val server_add_file : string -> unit Import.Deferred.t
+
+
+(* Client Stuff *)
+val client_connect :
+  string -> ftclient
+
+val client_get_file : ftclient -> string -> string
