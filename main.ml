d27d30e489365b0fb3bdcb42574d0e784c03cfc5
Charles Yu <charles.yu108@gmail.com>
2017-12-03 15:14:25 -0500

    state to_string dumps and imports

--- main.ml
+++ main.ml
@@ -5,3 +5,17 @@
 (* If differences on either this machine or the other, transfer handles updates *)
 (* GUI displays all stuff *)
 (* Crypto encrpyts files and user info/ connection-establishing processes *)
+
+let main () =
+  ANSITerminal.(print_string [green]
+                  "\n\nWelcome to Hump Drive. \n");
+  print_endline "Searching for
+  print_string  "> ";
+  match read_line () with
+  | exception End_of_file -> ()
+  | s -> print_endline "Hello"
+
+
+
+
+let () = main ()
