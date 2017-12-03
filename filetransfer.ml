280a89445f2ab6a76bd28378ff8ce570ed469c4d
sdd48 <sdd48@cornell.edu>
2017-12-03 15:40:22 -0500

    Cleanup filetransfer

--- filetransfer.ml
+++ filetransfer.ml
@@ -1,5 +1,6 @@
 open Async
 open Async_extra.Tcp
+open Async_extra
 
 type ftclient =
   (Tcp_file.Client.t, Base.Exn.t) Core.Result.t Async_extra.Import.Deferred.t
@@ -9,24 +10,21 @@ let port = 12345
 
 let create_server fname =
   let server_callback addr _ write =
-    print_string "Got Client!";
+    print_string "Got Client!\n";
     Socket.Address.Inet.addr addr |> Unix.Inet_addr.to_string |> print_endline;
-    let f = Reader.open_file fname in
-    f >>= fun fd ->
-    Async_extra.Import.Writer.transfer write (Reader.pipe fd)
-      (fun s -> Async_extra.Import.Writer.write write s) >>= fun () ->
+    Reader.open_file fname >>= fun fd ->
+    Import.Writer.transfer write (Reader.pipe fd)
+      (fun s -> Import.Writer.write write s) >>= fun () ->
     print_string "Finished Transferring!";
-    Async_extra.Import.Writer.close write
+    Import.Writer.close write
   in
   Server.create (on_port port) server_callback
 
 
-
-
 let client_connect_read host fname =
-  connect (to_host_and_port host port) >>= fun (sock, reader, _) ->
-  print_string "Connected to server!";
-  let f = Writer.open_file fname in
-  f >>= fun fd -> Async_extra.Import.Reader.transfer reader (Writer.pipe fd) >>= fun () ->
+  connect (to_host_and_port host port) >>= fun (_ , reader, _) ->
+  print_string "Connected to server!\n";
+  Writer.open_file fname >>= fun fd ->
+  Import.Reader.transfer reader (Writer.pipe fd) >>= fun () ->
   print_string "Finished receiving!";
-  Async_extra.Import.Reader.close reader
+  Import.Reader.close reader
