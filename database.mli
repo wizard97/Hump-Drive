0196ccae467dd2e4834949448d655f9012efa7e6
sdd48 <sdd48@cornell.edu>
2017-12-04 14:17:56 -0500

    Fixed database

--- database.mli
+++ database.mli
@@ -1,14 +1,13 @@
 (* Represents the database's internal state *)
 
 module OUnix = Unix
-open Async
 
 type state_info
 type dir_path
 
-val state_for_dir : dir_path -> state_info Deferred.t
+val state_for_dir : dir_path -> state_info Async.Deferred.t
 
-val update_state : state_info -> state_info Deferred.t
+val update_state : state_info -> state_info Async.Deferred.t
 
 val to_string : state_info -> dir_path
 
