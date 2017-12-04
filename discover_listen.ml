485958fe289ac2567dea8f85382bcba068ffb7ee
Aaron <daw268@cornell.edu>
2017-12-04 15:57:27 -0500

    Cleaned up discovery interface

--- discover_listen.ml
+++ discover_listen.ml
@@ -3,6 +3,9 @@ open Async
 open Async_extra
 open Peer_discovery
 
+let discovered addr msg =
+  print_string addr;
+  print_string msg
 
-let _ = Peer_discovery.listen "192.168.1.255"
+let _ = Peer_discovery.listen discovered
 let _ = Scheduler.go ()
--- peer_discovery.ml
+++ peer_discovery.ml
@@ -7,11 +7,15 @@ open Async
 open Async_extra
 open Async_unix
 open Crypto
+open Communication
+
+
+type listen_state = Fd.t
 
 (* Read public key from some stored file *)
 let read_public_key = 0
 
-
+let port = 30301
 
 (* Retrieve the public key as a string *)
 let packet =
@@ -29,10 +33,9 @@ let add_user adr s =
 (* Send universal broadcast of this device's
  * Note: 255.255.255.255 triggers te device's broadcast address. *)
 let broadcast () =
-  print_endline "called broadcast";
-  let addr_port = Async_extra.Import.Socket.Address.Inet.create_bind_any 12345 in
+  let addr_port = Async_extra.Import.Socket.Address.Inet.create_bind_any port in
   let addr1 = Unix_syscalls.Inet_addr.of_string "255.255.255.255" in
-  let addr = Async_extra.Import.Socket.Address.Inet.create addr1 12345 in
+  let addr = Async_extra.Import.Socket.Address.Inet.create addr1 port in
   let s = Socket.create Socket.Type.udp in
   Socket.connect s addr_port >>= fun s' ->
     print_endline "got socket";
@@ -51,19 +54,16 @@ let broadcast () =
 
 (* Send out UDP packet to alert network of this device
  * Only contains the public key of the device *)
-let listen adr =
-  print_endline "Called listen";
-  let addr1 = Async_unix.Unix_syscalls.Inet_addr.of_string "0.0.0.0" in
-  let adr_port = Async_extra.Import.Socket.Address.Inet.create_bind_any 12345 in
-    print_endline "Listening...";
+let listen dcallback =
+  let adr_port = Async_extra.Import.Socket.Address.Inet.create_bind_any port in
   Udp.bind adr_port >>= fun s ->
-    print_endline "got socket";
     let fd = Async_extra.Import.Socket.fd s in
-      let callback buf adr =
-        let adr' = Async_extra.Import.Socket.Address.Inet.to_string adr in
-        add_user adr' (Core.Iobuf.to_string buf);
-        print_endline "Received all the data" in
-  Udp.recvfrom_loop fd callback
+    let callback buf adr =
+      let adr' = Async_extra.Import.Socket.Address.Inet.to_string adr in
+      print_endline ("Discovered peer: "^adr');
+      dcallback adr' (Core.Iobuf.to_string buf)
+    in
+    Udp.recvfrom_loop fd callback >>= fun () -> Deferred.return fd
 
 
  (* Udp.bind addr >>= Async_extra.Import.Socket.listen
