c3e40d6af2cae17713f5813f11fa3d31e8c1cfd9
Charles Yu <charles.yu108@gmail.com>
2017-12-05 00:29:35 -0500

    database.ml comment updates

--- database.ml
+++ database.ml
@@ -1,31 +1,20 @@
-(* Done to differentiate between OCaml's default Unix module and JaneStreet's Async.Unix *)
 open Unix
 module OUnix = Unix
 open Async
 
+(* Modules used to maintain our files_to_info and update_queue structures *)
 module StringSet = Set.Make(String)
 module FileMap = Map.Make(String)
 
-<<<<<<< HEAD
-
-type file_hash = int
-type dir_path = string
-type update_queue = StringSet.t (* List of files that need to be updated *)
-type last_modified = float (* Last modified timestamp of a file *)
-
-(* Dictionary of file names to a tuple of its hash, last modified timestamp *)
-=======
 type dir_path = string
 type file_hash = int
 type update_queue = StringSet.t
 type last_modified = float
->>>>>>> 46ecab66342951e8fbdf79677fa94862c39d3b43
+
+(* Maps file names to file meta data of the files currently tracked *)
 type files_to_info = (file_hash * last_modified) FileMap.t
 
-
-(* Requires: [string] of file name
- * Returns: [int] that is the hash calculated for that file
- *)
+(* [compute_hash s] is the hash calculated for a string [s] *)
 let compute_hash s =
   let hash = ref 0 in
   let update_hash c =
@@ -34,17 +23,15 @@ let compute_hash s =
     hash := h' land h'
   in String.iter (update_hash) s; !hash
 
-
-(* Record with information about a state *)
+(* Internal state *)
 type state_info = {dir_path : dir_path;
                    files_to_info : files_to_info;
                    last_modified : last_modified;
                    update_queue : update_queue}
 
 
-(* Requires: [list] and [handle] of a directory path
- * Returns:
- *)
+(* [get_dir_contents acc h] returns a list of contents contained in the directory
+ * specified by [h] *)
 let rec get_dir_contents acc h =
   Async.Unix.readdir_opt h >>= (fun s ->
     match s with
@@ -52,9 +39,7 @@ let rec get_dir_contents acc h =
     | None -> Async.Unix.closedir h >>= (fun () -> Deferred.return (acc))
   )
 
-(* Requires: [string] of file path
- * Returns: [Deferred int] of the hash of the file
-*)
+(* Opens the file denoted by [fpath] and hashes its contents *)
 let hash_file fpath =
   Reader.open_file fpath >>= (fun rdr ->
     Reader.pipe rdr |> Async_unix.Import.Pipe.read >>=
@@ -64,35 +49,24 @@ let hash_file fpath =
         | `Eof -> failwith "Unsupported")
     )
 
-
-(* Requires: [string] of file path
- * Returns: [bool] comparing the equality 
-*)
+(* Returns whether the file denoted by [fpath] is a regular file *)
 let is_reg_file fpath =
   let fdesc = OUnix.openfile fpath [O_RDONLY; O_NONBLOCK] 644 in
   let stats = OUnix.fstat fdesc in
   stats.st_kind = S_REG
 
-
-(* Requires: [string] of file
- * Returns: [float] of last modified time of the file
-*)
+(* Returns the last modified time of the file denoted by [path] *)
 let last_modtime path =
   let fdesc = OUnix.openfile path [O_RDONLY; O_NONBLOCK] 644 in
   let stats = OUnix.fstat fdesc in
   stats.st_mtime
 
-
-(* Requires: [string] of directory path
- * Returns: [string list] of all the file names that are not directories
-*)
+(* Gets a list of only the regular files in the directory [dir_path] *)
 let files_in_dir dir_path =
   let handle = OUnix.opendir dir_path in
   get_dir_contents [] handle >>= fun lst ->
   Deferred.return (List.filter (fun f -> is_reg_file (dir_path^Filename.dir_sep^f)) lst)
 
-
-(* .mli *)
 let state_for_dir dir_path =
   (files_in_dir dir_path) >>=
   fun filenames ->
@@ -141,33 +115,30 @@ let update_file_info st =
   List.fold_left (fun acc x -> changed_files dir_path acc x)
         (Deferred.return (file_binds, queue)) fnames_to_modtimes
 
-
-        (* .mli *)
 let update_state st =
   let dir_path = st.dir_path in
   let new_modtime = last_modtime dir_path in
   if new_modtime <> st.last_modified then
     update_file_info st >>= fun (binds,queue) -> Deferred.return
       {st with files_to_info = binds; update_queue = queue; last_modified = new_modtime}
-    else Deferred.return st
+  else Deferred.return st
 
+(* Looks up a [file] in [st]'s files_to_info *)
 let lookup_file file st = FileMap.find file st.files_to_info
 
+(* Compares two versions of the same file [f] in [st1] and [st2]. Returns true if
+ * the version of [st2] is newer than that of [st1 ]*)
 let cmp_file_versions st1 st2 f =
   let h1,t1 = lookup_file f st1 in
   let h2,t2 = lookup_file f st2 in
   (t2 > t1) && (h2 <> h1)
 
-
-    (* .mli *)
 let files_to_request st_curr st_inc =
   let files_to_add = StringSet.diff st_inc.update_queue st_curr.update_queue in
   let conflict_files = (StringSet.inter st_curr.update_queue st_inc.update_queue) in
   let poss_ups = StringSet.filter (cmp_file_versions st_curr st_inc) conflict_files in
   StringSet.union poss_ups files_to_add |> StringSet.elements
 
-
-    (* .mli *)
 let acknowledge_file_recpt st fname =
   let fpath = st.dir_path ^ Filename.dir_sep ^ fname in
   let modtime = last_modtime fpath in
@@ -179,10 +150,6 @@ let acknowledge_file_recpt st fname =
                     files_to_info = filemap;
                     last_modified = dir_lastmodtime}
 
-
-      (* .mli *)
 let to_string (st : state_info) = Marshal.to_string st []
 
-
-    (* .mli *)
 let from_string (s : string) : state_info = Marshal.from_string s 0
