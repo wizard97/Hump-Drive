46ecab66342951e8fbdf79677fa94862c39d3b43
Charles Yu <charles.yu108@gmail.com>
2017-12-04 23:54:17 -0500

    database.mli comments

--- database.mli
+++ database.mli
@@ -14,11 +14,18 @@ val state_for_dir : string -> state_info Async.Deferred.t
  * the tracked directory synced with the database state_info represented by [st] *)
 val update_state : state_info -> state_info Async.Deferred.t
 
-
+(* [files_to_request st1 st2] compares two state_info's and returns a string list
+ * of files that st1 needs to request from st2 in order to be synced up to date. *)
 val files_to_request : state_info -> state_info -> string list
 
+(* [acknowledge_file_recpt st fname] Returns a deferred state_info that represents
+ * an updated [st] for the file of name [fname]. This function is intended to be
+ * used as a callback to update a machine's state_info when a requested file
+ * denoted by [fname] is successfully received by the machine  *)
 val acknowledge_file_recpt : state_info ->  string -> state_info Async.Deferred.t
 
+(* Exports a state_info to a non-human readable string *)
 val to_string : state_info -> string
 
+(* Imports a string representing a state_info as a state_info *)
 val from_string : string -> state_info
