bf0a694296106d6a05aca639173afba16de06976
Zack Brienza <zb43@cornell.edu>
2017-12-07 00:05:12 -0500

    Removed update queue in state.ml

--- state.ml
+++ state.ml
@@ -3,12 +3,12 @@ module OUnix = Unix
 open Async
 
 (* Modules used to maintain our files_to_info and update_queue structures *)
-module StringSet = Set.Make(String)
+(* module StringSet = Set.Make(String) *)
 module FileMap = Map.Make(String)
 
 type dir_path = string
 type file_hash = int
-type update_queue = StringSet.t
+(* type update_queue = StringSet.t *)
 type last_modified = float
 
 (* Maps file names to file meta data of the files currently tracked *)
@@ -26,8 +26,7 @@ let compute_hash s =
 (* Internal state *)
 type state_info = {dir_path : dir_path;
                    files_to_info : files_to_info;
-                   last_modified : last_modified;
-                   update_queue : update_queue}
+                   last_modified : last_modified;}
 
 
 let root_dir s =
@@ -83,48 +82,43 @@ let state_for_dir dir_path =
         let file_info = List.map2 (fun a b -> (a,b)) filehashes filemodtimes in
         let file_mappings = List.fold_left2 (fun acc fname finfo -> FileMap.add fname finfo acc)
           FileMap.empty filenames file_info in
-        let update_queue = FileMap.fold (fun key _ acc -> StringSet.add key acc) file_mappings StringSet.empty in
         let time = last_modtime dir_path in
         Deferred.return
         {dir_path = dir_path;
          files_to_info = file_mappings;
-         last_modified = time;
-         update_queue = update_queue}
+         last_modified = time;}
 
 (* Updates a filemap and queue given current file info  *)
 let changed_files dir_path acc (fname, modtime) =
-  acc >>= fun (file_map,queue) ->
+  acc >>= fun (file_map) ->
   try
     let _, stored_modtime = FileMap.find fname file_map in
     if modtime <> stored_modtime then
       hash_file (dir_path^Filename.dir_sep^fname) >>=
       fun new_hash -> Deferred.return
-      (FileMap.add fname (new_hash, modtime) file_map, StringSet.add fname queue)
-    else Deferred.return (file_map, queue)
+      (FileMap.add fname (new_hash, modtime) file_map)
+    else Deferred.return (file_map)
   with Not_found ->
     hash_file (dir_path^Filename.dir_sep^fname) >>=
     fun new_hash -> Deferred.return
-    (FileMap.add fname (new_hash, modtime) file_map, StringSet.add fname queue)
+    (FileMap.add fname (new_hash, modtime) file_map)
 
 (* Given a st, returns an updated filebinding and queue. Helper for update_state  *)
 let update_file_info st =
   let dir_path = st.dir_path in
   let file_binds = st.files_to_info in
-  let queue = st.update_queue in
-  files_in_dir dir_path >>=
-    fun curr_dir_contents ->
-  let fnames_to_modtimes = List.map (fun fil ->
-      (fil, last_modtime (dir_path^Filename.dir_sep^fil))) curr_dir_contents in
-
+  files_in_dir dir_path >>= fun curr_dir_contents ->
+    let fnames_to_modtimes = List.map (fun fil ->
+        (fil, last_modtime (dir_path^Filename.dir_sep^fil))) curr_dir_contents in
   List.fold_left (fun acc x -> changed_files dir_path acc x)
-        (Deferred.return (file_binds, queue)) fnames_to_modtimes
+        (Deferred.return file_binds) fnames_to_modtimes
 
 let update_state st =
   let dir_path = st.dir_path in
   let new_modtime = last_modtime dir_path in
   if new_modtime <> st.last_modified then
-    update_file_info st >>= fun (binds,queue) -> Deferred.return
-      {st with files_to_info = binds; update_queue = queue; last_modified = new_modtime}
+    update_file_info st >>= fun binds -> Deferred.return
+      {st with files_to_info = binds; last_modified = new_modtime}
   else Deferred.return st
 
 (* Looks up a [file] in [st]'s files_to_info *)
@@ -138,23 +132,29 @@ let cmp_file_versions st1 st2 f =
   (t2 > t1) && (h2 <> h1)
 
 let files_to_request st_curr st_inc =
-  let files_to_add = StringSet.diff st_inc.update_queue st_curr.update_queue in
+  let curr_binds = st_curr.files_to_info in
+  let inc_binds = st_inc.files_to_info in
+  FileMap.fold (fun k _ acc ->
+      if FileMap.mem k curr_binds then acc
+      else if (cmp_file_versions st_curr st_inc k)
+      then k::acc else acc) inc_binds []
+
+  (* let files_to_add = StringSet.diff st_inc.update_queue st_curr.update_queue in
   let conflict_files = (StringSet.inter st_curr.update_queue st_inc.update_queue) in
   let poss_ups = StringSet.filter (cmp_file_versions st_curr st_inc) conflict_files in
-  StringSet.union poss_ups files_to_add |> StringSet.elements
+  StringSet.union poss_ups files_to_add |> StringSet.elements *)
 
 let acknowledge_file_recpt st fname =
   let fpath = st.dir_path ^ Filename.dir_sep ^ fname in
   let modtime = last_modtime fpath in
-  let q = StringSet.remove fname (st.update_queue) in
   hash_file fpath >>= fun hash ->
     let filemap = FileMap.add fname (hash, modtime) (st.files_to_info) in
     let dir_lastmodtime = last_modtime st.dir_path in
-    Deferred.return {st with update_queue = q;
+    Deferred.return {st with
                     files_to_info = filemap;
                     last_modified = dir_lastmodtime}
 
-let to_string (st : state_info) = Marshal.to_string st [No_sharing] |> String.escaped
+let to_string (st : state_info) = Marshal.to_string st [] |> String.escaped
 
 let from_string (s : string) : state_info = Marshal.from_string (Scanf.unescaped s) 0
 
