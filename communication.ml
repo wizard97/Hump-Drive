4d1b5838550866bf44f5661395a0dc496d02c6b1
sdd48 <sdd48@cornell.edu>
2017-12-07 01:58:30 -0500

    First test at incorporating crypto

--- communication.ml
+++ communication.ml
@@ -5,6 +5,19 @@ open Async_extra.Import.Writer
 open Crypto
 
 
+
+
+
+
+
+
+
+
+let pu = Crypto.key_from_string "371231380513840174425645412377223429621325167085653107566134660546088480210237155858539524043527005322034361505765820564154683030558303344226815372571949747793522185924674417872780071492929729165727870590934771223134363607914632879286523420851703728462705568934768942031062709035994744032500160448359337994020732662070606000346629729130536739490941894427609496909410526964662159120133362021593735851"
+let pr = Crypto.key_from_string "9499797956224051813010422366384444205472597967599260452101133929924446975107426183598141074532212097605489756758113451373599490276191190211067292650467286484856886681775881130162455448352098352597201"
+
+
+
 type message = State of string | Filerequest of string
 
 (* ip, key *)
@@ -15,7 +28,7 @@ type server = (Socket.Address.Inet.t, int) Async_extra.Tcp.Server.t
 type conn_state = Socket.Address.Inet.t*Reader.t*Writer.t
 
 
-let port = 12345
+let port = 12346
 
 
 let cmp_sub s cmd =
@@ -30,23 +43,37 @@ let msg_to_string = function
   | State s -> "state:"^s^"\n"
   | Filerequest s -> "freq:"^s^"\n"
 
-
+(*)
 let transfer_file fname (addr,read,write) =
   Reader.open_file fname >>= fun fd ->
   Writer.transfer write (Reader.pipe fd)
     (fun s -> Writer.write write s) >>= fun () ->
   Writer.flushed write >>= fun () ->
   print_string "Finished Transferring!";
-  Reader.close fd
+  Reader.close fd   *)
 
 
+let transfer_file fname (addr,read,write) =
+  Reader.open_file fname >>= fun r ->
+    let prd = Reader.pipe r in
+    Pipe.read prd >>= fun a -> match a with
+      | `Ok s -> Crypto.encrypt_and_chunk s pu |> Writer.write write |> return
+      | `Eof -> Reader.close r
 
+(*
 let recv_file fdest (addr,read,write) =
   Writer.open_file fdest >>= fun fd ->
   Reader.transfer read (Writer.pipe fd) >>= fun () ->
   print_string "Finished receiving!";
-  Writer.close fd
+  Writer.close fd *)
 
+
+let recv_file fdest (addr,read,write) =
+  Writer.open_file fdest >>= fun w ->
+  let prd = Reader.pipe read in
+  Pipe.read prd >>= fun a -> match a with
+    | `Ok s -> Crypto.decrypt_chunked s pu pr |> Writer.write w |> return
+    | `Eof -> Writer.close w
 
 
 let process_cmd s cstate pr (hookup : (conn_state -> peer -> message -> unit Async.Deferred.t)) =
